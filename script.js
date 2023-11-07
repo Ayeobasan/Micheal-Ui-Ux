de2b58c75e849dc4bf4c5193306f7122e92e6b02
Ayeobasan John <johnayeobasan@gmail.com>
2023-11-07 23:26:37 +0100

    second

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function navigateToPage(pageURL) {
-    window.location.href = pageURL;
+  window.location.href = pageURL;
 }
 function openNav() {
-    document.getElementById("mySidebar").style.width = "450px"; // Width for larger screens
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
-
-function validatePassword() {
-    const passwordInput = document.getElementById("password");
-    const errorMessage = document.getElementById("error-message");
-    const password = passwordInput.value;
-
-    if (password === "Micheal098") {
-        navigateToPage("projects.html");
-    } else {
-        errorMessage.textContent = "Incorrect Password. Try again.";
-        passwordInput.value = "";
-    }
+  document.getElementById("mySidebar").style.width = "450px"; // Width for larger screens
 }
 
-document.getElementById("password-form")
-    .addEventListener("submit", function (event) {
-        event.preventDefault();
-        validatePassword();
-    });
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+
+function validatePassword() {
+  const passwordInput = document.getElementById("password");
+  const errorMessage = document.getElementById("error-message");
+  const password = passwordInput.value;
+
+  if (password === "Micheal098") {
+    navigateToPage("projects.html");
+  } else {
+    errorMessage.textContent = "Incorrect Password. Try again.";
+    passwordInput.value = "";
+  }
+}
+
+const numCursors = 2; // Adjust the number of trailing cursors
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+    // Update the position of the cursor
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+});
