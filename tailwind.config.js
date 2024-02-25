3c0538ef67c6f0f49d740e385361e3ca0af0066e
Ayeobasan John <johnayeobasan@gmail.com>
2024-02-25 21:53:44 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+    },
+    screens: {
+      xxs: "300px",
+      xs: "400px",
+      sm: "480px",
+      xm: "640px",
+      md: "768px",
+      xd: "960px",
+      lg: "992px",
+      xlg: "1110px",
+      xl: "1200px",
+    },
   },
   plugins: [],
-}
+};
